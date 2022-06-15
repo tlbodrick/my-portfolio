15051b344f0642a1cc1b3f34c0504110aabcd63a
Tamera <tlbodrick@gmail.com>
2022-06-15 11:40:16 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#779AAF',
+        'custom-orange': '#BD6424'
+      },
+    },
+  },
+  plugins: [],
+}
